51f5dd8f52a8709926b5f52fa44e69975ca50d1b
mpetrasin <milos.petrasin@gmail.com>
2023-03-15 20:38:48 +0100

    Update commit-stage.yml

--- src/test/java/com/polarbookshop/dispatcherservice/DispatchingFunctionsIntegrationTests.java
+++ src/test/java/com/polarbookshop/dispatcherservice/DispatchingFunctionsIntegrationTests.java
@@ -1,5 +1,6 @@
 package com.polarbookshop.dispatcherservice;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.function.context.FunctionCatalog;
@@ -10,6 +11,7 @@ import reactor.test.StepVerifier;
 import java.util.function.Function;
 
 @FunctionalSpringBootTest
+@Disabled("These tests are only necessary when using the functions alone (no bindings)")
 class DispatchingFunctionsIntegrationTests {
 
     @Autowired
